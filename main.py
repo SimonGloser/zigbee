245c1c9e05fa16627579e3bc7905aba71c1e31bb
simongloser1985 <simon.gloser@googlemail.com>
2021-05-27 17:08:10 +0200

    enabled read com4

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(f'Hello World')
+import serial
+
+
+ser = serial.Serial('com4', baudrate = 38400, parity= serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
+
+while 1:
+    a=ser.readline()
+    print(a)
+
