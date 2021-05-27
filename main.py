5a2a5090817e9cfb6efd71902e318c64e817bf67
simongloser1985 <simon.gloser@googlemail.com>
2021-05-27 17:29:35 +0200

    enabled terminal print from com4

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import serial
-
+"""
+Dieser Teil wird spaeter in eine seperate funktion ausgelagert. Dieser stand dient nur zu demozwecken
+"""
 
 ser = serial.Serial('com4', baudrate = 38400, parity= serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE)
 
